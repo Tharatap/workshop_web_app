71ea5908f85bb8054d5260d36be3f670ac3336ad
Tharatap Tape <tharatap.t@ku.th>
2026-09-13 15:33:42 +0700

    [dev] database_endpoint MapGet and post endpoint

--- todo-workshop/TodoApi/Program.cs
+++ todo-workshop/TodoApi/Program.cs
@@ -28,67 +28,97 @@ app.UseHttpsRedirection();
 
 var todoGroup = app.MapGroup("/api/todos").WithTags("Todos");
 
-var todos = new List<TodoGetDto>
-{
-    new(1,"Tharatap",true),
-    new(2,"Thanakorn",true),
-    new(3,"pun",false),
-};
-todoGroup.MapGet("/",() => Results.Ok(todos));
+#region In-Memory Endpoints
+
+// var todos = new List<TodoGetDto>
+// {
+//     new(1,"Tharatap",true),
+//     new(2,"Thanakorn",true),
+//     new(3,"pun",false),
+// };
+// todoGroup.MapGet("/",() => Results.Ok(todos));
 
 
-todoGroup.MapGet("/api/todos/{id}", (int id) =>
-{
+// todoGroup.MapGet("/api/todos/{id}", (int id) =>
+// {
 
-   var todo = todos.FirstOrDefault(t => t.Id == id);
-   return todo is not null ? Results.Ok(todo) : Results.NotFound();
-});
+//    var todo = todos.FirstOrDefault(t => t.Id == id);
+//    return todo is not null ? Results.Ok(todo) : Results.NotFound();
+// });
 
-todoGroup.MapPost("/api/todos",(TodoPostDto dto) =>
-{
-    var nextID = todos.Count == 0 ? 1 : todos.Max(t => t.Id) + 1;
-    var todo = new TodoGetDto(nextID,dto.Title,false);
-    todos.Add(todo);
-    return Results.Created($"/api/todos/{todo.Id}",todo);
-});
+// todoGroup.MapPost("/api/todos",(TodoPostDto dto) =>
+// {
+//     var nextID = todos.Count == 0 ? 1 : todos.Max(t => t.Id) + 1;
+//     var todo = new TodoGetDto(nextID,dto.Title,false);
+//     todos.Add(todo);
+//     return Results.Created($"/api/todos/{todo.Id}",todo);
+// });
 
-todoGroup.MapPut("/api/todos/{id}",(int id, TodoPutDto dto)=>
-{
-    try
-    {
-        var index = todos.FindIndex(t => t.Id == id);
-        if(index == -1) return Results.NotFound();
+// todoGroup.MapPut("/api/todos/{id}",(int id, TodoPutDto dto)=>
+// {
+//     try
+//     {
+//         var index = todos.FindIndex(t => t.Id == id);
+//         if(index == -1) return Results.NotFound();
 
-        todos[index] = todos[index] with
-        {
-        Title = dto.Title,
-        IsCompleted = dto.IsCompleted
-        };
-        return Results.Ok(todos[index]);
-    }
-    catch (Exception ex){
-       return Results.Problem(ex.Message);
-    }
+//         todos[index] = todos[index] with
+//         {
+//         Title = dto.Title,
+//         IsCompleted = dto.IsCompleted
+//         };
+//         return Results.Ok(todos[index]);
+//     }
+//     catch (Exception ex){
+//        return Results.Problem(ex.Message);
+//     }
     
-});
+// });
 
-todoGroup.MapDelete("/api/todos/{id}", (int id) =>
-{
-    try{
-        var todo = todos.FirstOrDefault(t => t.Id == id);
-        if (todo is null) return Results.NotFound();
+// todoGroup.MapDelete("/api/todos/{id}", (int id) =>
+// {
+//     try{
+//         var todo = todos.FirstOrDefault(t => t.Id == id);
+//         if (todo is null) return Results.NotFound();
         
-        todos.Remove(todo);
-        return Results.NoContent();
-    }
-    catch (ArgumentException ex)
+//         todos.Remove(todo);
+//         return Results.NoContent();
+//     }
+//     catch (ArgumentException ex)
+//     {
+//         return Results.Problem("Is non");
+//     }
+//     catch(Exception ex)
+//     {
+//         return Results.Problem(ex.Message);
+//     }
+// });
+
+#endregion
+
+#region Database Enpoints
+    todoGroup.MapGet("/",async(AppDbContext db)=>
     {
-        return Results.Problem("Is non");
-    }
-    catch(Exception ex)
+        var todos = await db.Todos.ToListAsync();
+        return todos.Count == 0 ? Results.NoContent() :  Results.Ok(todos);
+    });
+
+    todoGroup.MapPost("/",async(AppDbContext db,TodoPostDto dto) =>
     {
-        return Results.Problem(ex.Message);
-    }
-});
+        var lastTodo = await db.Todos.OrderByDescending(t => t.Id).FirstOrDefaultAsync();
+        var nextID = lastTodo is null ? 1 : lastTodo.Id + 1;
+        var todo = new TodoItem
+        {
+            Id = nextID,
+            Title = dto.Title,
+            IsCompleted = false,
+            CreatedAt = DateTime.UtcNow
+        };
+        db.Todos.Add(todo);
+        await db.SaveChangesAsync();
+
+        var todoGetDto = new TodoGetDto(todo.Id, todo.Title, todo.IsCompleted);
+        return Results.Created($"/{todo.Id}",todo);
+    });
+#endregion
 
 app.Run();
